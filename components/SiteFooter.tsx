df3024b146a4cf12c9c00802f05ab9c61a143ba5
fra.scavone88 <fra.scavone88@gmail.com>
2024-12-23 15:50:09 +0100

    fix: site footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -29,72 +29,60 @@ export const SiteFooter: FC<SiteFooterProps> = ({}) => {
     return (
         <footer
             id="contacts"
-            className="border-t bg-gradient-to-b from-gray-200 p-4 text-center dark:from-secondary"
+            className="flex flex-col-reverse md:flex-row items-center md:justify-between border-t bg-gradient-to-b from-gray-200 p-2 text-center dark:from-secondary"
         >
-            <h2 className="mb-2 font-mono text-2xl">{t("contacts")}</h2>
-            <div className="m-auto flex flex-col gap-8 border-b-2 pb-4 text-center md:w-1/3">
-                {t("cta")}
-                <a
-                    rel="noreferrer"
-                    target="_blank"
-                    href="mailto:fra.scavone88@gmail.com"
-                >
-                    <Button variant="default">e-mail</Button>
-                </a>
-                <div className="m-auto my-4 flex w-4/5 justify-around">
-                    {siteConfig.socials.map(({ name, link }) => (
-                        <TooltipProvider key={name}>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    <Link
-                                        href={link}
-                                        target="_blank"
-                                        rel="noreferrer"
+            <div className="gap-8 hidden md:flex">
+                {siteConfig.socials.map(({ name, link }) => (
+                    <TooltipProvider key={name}>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Link
+                                    href={link}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div
+                                        className={buttonVariants({
+                                            size: "icon",
+                                            variant: "ghost",
+                                        })}
                                     >
-                                        <div
-                                            className={buttonVariants({
-                                                size: "icon",
-                                                variant: "ghost",
-                                            })}
-                                        >
-                                            {name === "github" ? (
-                                                <GitHubLogoIcon className="h-8 w-8" />
-                                            ) : name === "linkedin" ? (
-                                                <LinkedInLogoIcon className="h-8 w-8" />
-                                            ) : name === "gitlab" ? (
-                                                <Icon
-                                                    icon="ph:gitlab-logo"
-                                                    className="h-8 w-8"
-                                                />
-                                            ) : null}
-                                            <span className="sr-only">
-                                                {name}
-                                            </span>
-                                        </div>
-                                    </Link>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    {capitalizeFirstLetter(name)}
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                    ))}
-                </div>
-                <a
-                    rel="noreferrer"
-                    href="#top"
-                    className="flex items-center justify-center"
-                >
-                    <DoubleArrowUpIcon className="mr-4 inline " />
-                    <span className="underline">{t("back-to-top")}</span>
-                    <DoubleArrowUpIcon className="ml-4 inline " />
-                </a>
+                                        {name === "github" ? (
+                                            <GitHubLogoIcon className="h-8 w-8" />
+                                        ) : name === "linkedin" ? (
+                                            <LinkedInLogoIcon className="h-8 w-8" />
+                                        ) : name === "gitlab" ? (
+                                            <Icon
+                                                icon="ph:gitlab-logo"
+                                                className="h-8 w-8"
+                                            />
+                                        ) : null}
+                                        <span className="sr-only">{name}</span>
+                                    </div>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                {capitalizeFirstLetter(name)}
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                ))}
             </div>
+            {/* </div> */}
             <p className="mt-2 text-gray-400">
                 &copy; {`${new Date().getFullYear()} Built with `}
                 <HeartFilledIcon className="inline" color="red" />
                 {` by Francesco Scavone`}
             </p>
+            <a
+                rel="noreferrer"
+                href="#top"
+                className="flex items-center justify-center"
+            >
+                <DoubleArrowUpIcon className="mr-4 inline " />
+                <span className="underline">{t("back-to-top")}</span>
+                <DoubleArrowUpIcon className="ml-4 inline " />
+            </a>
         </footer>
     );
 };
